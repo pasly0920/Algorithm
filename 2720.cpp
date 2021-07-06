4d86f8b9b77463cd4e6f87cc7bf91e556cce432b
pasly0920 <inwoo920621@gmail.com>
2021-07-06 11:44:41 +0900

    greedy

---
+++ 2720.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T,C;
+    int q, d, n, p;
+    cin>>T;
+    for(int i=0; i<T;i++)
+    {
+        cin>>C;
+        q=C/25;
+        C-=25*q;
+        d=C/10;
+        C-=10*d;
+        n=C/5;
+        C-=5*n;
+        p=C;
+        cout<<q<<" "<<d<<" "<<p<<" "<<n<<endl;
+    }
+    return 0;
+}
