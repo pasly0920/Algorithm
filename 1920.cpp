d30323c540b65d8a060684e12ad5ca2bad7c0ab7
pasly0920 <inwoo920621@gmail.com>
2022-05-27 14:56:08 +0900

    BOJ 1920 수찾기

--- 1920.cpp
+++ 1920.cpp
@@ -1,29 +1,32 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+
 using namespace std;
-int main()
-{
-    ios_base :: sync_with_stdio(false); 
-    cin.tie(NULL); 
+int N, M;
+vector<int> num;
+vector<int> target;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
     cout.tie(NULL);
-    int N;
+    
     cin>>N;
-    vector<int> A(N);
-    for(int i=0;i<N;i++)
-        cin>>A[i];
-    sort(A.begin(), A.end());
-    int M;
+    num.assign(N, 0);
+    for(int i=0; i<N; i++)
+        cin>>num[i];
+    sort(num.begin(), num.end());
+
     cin>>M;
-    vector<int> B(M);
-    for(int i=0;i<M;i++)
-        cin>>B[i];
-    for(int i=0;i<M;i++)
-    {
-        if(A[lower_bound(A.begin(), A.end(), B[i])-A.begin()]==B[i])
-            cout<<1<<'\n';
-        else
-            cout<<0<<'\n';
+    target.assign(M, 0);
+    for(int i=0; i<M; i++)
+        cin>>target[i];
+
+    for(int i=0; i<M; i++){
+        int targetNum = target[i];
+        cout<<int(binary_search(num.begin(), num.end(), targetNum))<<'\n';
     }
+
     return 0;
-}   
+}
